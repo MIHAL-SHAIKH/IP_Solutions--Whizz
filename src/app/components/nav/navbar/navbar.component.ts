f059b7b9ba50cdaeb2e61bacc0c8372c1c5bf629
Mihal Shaikh <mehal.shaikh@outlook.com>
2024-08-26 18:40:09 +0530

    create: search bar sugestions

--- src/app/components/nav/navbar/navbar.component.ts
+++ src/app/components/nav/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { footer } from 'src/app/demoData';
 
 @Component({
@@ -7,9 +7,45 @@ import { footer } from 'src/app/demoData';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  courses :any = footer.courses;
-  constructor(){
-    console.log(footer,'footer')
+  courses: any = footer.courses;
+  searchQuery: string = '';
+  showDropdown: boolean = false;
+  // searchQuery
+
+
+  searchItems: string[] = [
+    'Angular Tutorial',
+    'Responsive Design',
+    'TypeScript Basics',
+    'Bootstrap Mega Menu',
+    'JavaScript Best Practices',
+    'Frontend Development'
+  ];
+
+  filteredItems: string[] = [...this.searchItems];
+
+// searchQuery: string = '';
+recommendations: string[] = ['History 1', 'History 2', 'Recommendation 1'];
+
+selectRecommendation(item: string) {
+  this.searchQuery = item;
+  console.warn('clicked',item)
+}
+
+  constructor() {
+    console.log(footer, 'footer');
   }
 
+  filterResults(event: Event) {
+    const input = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredItems = this.searchItems.filter(item =>
+      item.toLowerCase().includes(input)
+    );
+  }
+
+  hideDropdown(): void {
+    setTimeout(() => {
+      this.showDropdown = false;
+    }, 150);
+  }
 }
